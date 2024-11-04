db81db8e332ded3d85851492c6d5a970d0cb148d
SabaXdev <sabakatamadze@gmail.com>
2024-11-04 14:23:44 +0400

    Change button functionality

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const fs = require('fs');
 const app = express();
+const { exec } = require('child_process')
 const PORT = 3000;
 
 app.use(cors());
@@ -11,15 +12,30 @@ app.use(express.json());
 
 app.post('/save-data', (req, res) => {
     const data = req.body;
+    const jsonFilePath = './GaSchedule.Console/user_data_new.json';
 
     // Save the JSON data to a file
-    fs.writeFile('user_data.json', JSON.stringify(data, null, 2), (err) => {
+    fs.writeFile(jsonFilePath, JSON.stringify(data, null, 2), (err) => {
         if (err) {
             console.error(err);
             res.status(500).json({ message: 'Failed to save data' });
         } else {
             res.status(200).json({ message: 'Data saved successfully' });
         }
+    });
+
+    // Change directory to GaSchedule.Console and run dotnet build and dotnet run
+    exec(`cd GaSchedule.Console && dotnet build && dotnet run`, (error, stdout, stderr) => {
+        if (error) {
+            console.error(`Error: ${error.message}`);
+            return res.status(500).send('Failed to run .NET project');
+        }
+        if (stderr) {
+            console.error(`Stderr: ${stderr}`);
+        }
+        console.log(`Stdout: ${stdout}`);
+        
+        res.send('Schedule generation process initiated');
     });
 });
 
